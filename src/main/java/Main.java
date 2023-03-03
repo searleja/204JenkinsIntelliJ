99d98a558eda8e20141ddd377433dbbc82708750
searleja <searlejarod@gmail.com>
2023-03-02 21:01:35 -0700

    done (hopefully)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,30 +10,31 @@ public class Main {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
 
-        // Reading data using readLine
+
+        System.out.println("welcome to the calculator\nEnter a command:");
+
         String val = reader.readLine();
 
         String[] vals = val.split(" ");
 
-        int ret = 0;
+        while (val != "done") {
 
-        if (vals[0] == "add") {
-            System.out.println(calculator.add(Integer.parseInt(vals[1]), Integer.parseInt(vals[2])));
-        }
-        else if (vals[0] == "subtract") {
-            System.out.println(calculator.subtract(Integer.parseInt(vals[1]), Integer.parseInt(vals[2])));
-        }
-        else if (vals[0] == "multiply") {
-            System.out.println(calculator.multiply(Integer.parseInt(vals[1]), Integer.parseInt(vals[2])));
-        }
-        else if (vals[0] == "divide") {
-            System.out.println(calculator.divide(Integer.parseInt(vals[1]), Integer.parseInt(vals[2])));
-        }
-        else if (vals[0] == "fib") {
-            System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(vals[1])));
-        }
-        else if (vals[0] == "binary") {
-            System.out.println(calculator.intToBinaryNumber(Integer.parseInt(vals[1])));
+            if (vals[0] == "add") {
+                System.out.println(calculator.add(Integer.parseInt(vals[1]), Integer.parseInt(vals[2])));
+            } else if (vals[0] == "subtract") {
+                System.out.println(calculator.subtract(Integer.parseInt(vals[1]), Integer.parseInt(vals[2])));
+            } else if (vals[0] == "multiply") {
+                System.out.println(calculator.multiply(Integer.parseInt(vals[1]), Integer.parseInt(vals[2])));
+            } else if (vals[0] == "divide") {
+                System.out.println(calculator.divide(Integer.parseInt(vals[1]), Integer.parseInt(vals[2])));
+            } else if (vals[0] == "fib") {
+                System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(vals[1])));
+            } else if (vals[0] == "binary") {
+                System.out.println(calculator.intToBinaryNumber(Integer.parseInt(vals[1])));
+            }
+            val = reader.readLine();
+
+            vals = val.split(" ");
         }
     }
 }
